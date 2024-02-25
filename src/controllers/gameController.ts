3ebcbb05ae8c43e29a290f1be440b328275f584d
jose orellana <daniel@emio.cl>
2024-02-25 13:12:20 -0500

    reto dd3

--- src/controllers/gameController.ts
+++ src/controllers/gameController.ts
@@ -1,23 +1,29 @@
-// gameController.ts
-
 import { Request, Response } from 'express';
+
+let estadoJuego = {
+  palabraSeleccionada: '',
+  tiempoSeleccion: Date.now(),
+  intentos: 0,
+  jugadas: 0,
+  victorias: 0
+};
 
 const palabras: string[] = ['getos', 'gatos', 'vocal', 'luzes', 'piano', 'ritmo', 'sabor', 'trenz', 'unido', 'virus'];
 
-export const adivinarPalabra = (req: Request, res: Response): Response => {
-  const intento: string = req.body.intento;
-  const palabraSeleccionada: string = req.palabraSecreta;
-  console.log("palabraselccionada", palabraSeleccionada)
-
-  if (intento.length !== 5) {
-    return res.status(400).json({ message: 'La palabra debe tener 5 letras.' });
-  }
-
-  let resultado: any[] = verificarPalabra(intento, palabraSeleccionada);
-
-  return res.json(resultado);
+const seleccionarNuevaPalabra = () => {
+  estadoJuego.palabraSeleccionada = palabras[Math.floor(Math.random() * palabras.length)];
+  estadoJuego.tiempoSeleccion = Date.now();
+  estadoJuego.intentos = 0;
 };
 
+const verificarTiempoParaNuevaPalabra = () => {
+  if (Date.now() - estadoJuego.tiempoSeleccion >= 300000) {
+    seleccionarNuevaPalabra();
+    estadoJuego.jugadas++;
+    return true;
+  }
+  return false;
+};
 
 const verificarPalabra = (intento: string, palabraSeleccionada: string): any[] => {
   let resultado: any[] = [];
@@ -32,3 +38,35 @@ const verificarPalabra = (intento: string, palabraSeleccionada: string): any[] =
   }
   return resultado;
 };
+
+
+export const adivinarPalabra = (req: Request, res: Response): Response => {
+  estadoJuego.intentos++;
+  const intento: string = req.body.intento;
+
+  const tiempoTranscurrido = Math.floor((Date.now() - estadoJuego.tiempoSeleccion) / 60000) + ':' + Math.floor(((Date.now() - estadoJuego.tiempoSeleccion) % 60000) / 1000).toString().padStart(2, '0');
+  if ((verificarTiempoParaNuevaPalabra() || estadoJuego.intentos >= 5) && intento != estadoJuego.palabraSeleccionada ) {
+    if(estadoJuego.intentos==5){
+      estadoJuego.jugadas++
+    }
+   console.log("quiero verficiar que el entre aca.")
+    const resultadoFinal = { jugadas: estadoJuego.jugadas, victorias: estadoJuego.victorias, tiempoTranscurrido, resultado: verificarPalabra(intento, estadoJuego.palabraSeleccionada) };
+    return res.json(resultadoFinal);
+  }
+
+  const resultado = verificarPalabra(intento, estadoJuego.palabraSeleccionada);
+
+  if (intento === estadoJuego.palabraSeleccionada) {
+    estadoJuego.victorias++;
+    seleccionarNuevaPalabra();
+    estadoJuego.jugadas++;
+    return res.json({ intento, resultado, jugadas: estadoJuego.jugadas, victorias: estadoJuego.victorias });
+  } else {
+    console.log("intentado de nuevo")
+    return res.json({ intento, resultado });
+  }
+};
+
+
+// Inicializar el juego
+seleccionarNuevaPalabra();
