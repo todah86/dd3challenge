548b40dc00d48bb3cd42d17ffc4a6537c0a8d9b6
jose orellana <daniel@emio.cl>
2024-02-26 15:18:44 -0500

    migrando todo a postgress

--- src/entity/User.ts
+++ src/entity/User.ts
@@ -4,13 +4,14 @@ import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
 export class User {
   constructor() {
     this.password = '';
+    this.username = '';
   }
   
   @PrimaryGeneratedColumn()
   id: number | undefined;
 
-  @Column()
-  username: string | undefined;
+  @Column({ type: 'varchar', length: 255 }) // Aquí usamos varchar para username
+  username: string;
 
   @Column()
   password: string;
